5b792c79bbaaa31ebb5ae87dad5ecdd44b0da27b
ajcantor1 <ajcantor1@gmail.com>
2021-02-07 20:19:15 -0500

    added styled components lib to gatsby config

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -2,6 +2,9 @@ var plugins = [{
       plugin: require('/home/jeremy/Desktop/projects/website/website/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
       options: {"plugins":[]},
     },{
+      plugin: require('/home/jeremy/Desktop/projects/website/website/node_modules/gatsby-plugin-styled-components/gatsby-ssr'),
+      options: {"plugins":[],"displayName":true,"fileName":true,"minify":true,"namespace":"","transpileTemplateLiterals":true,"pure":false},
+    },{
       plugin: require('/home/jeremy/Desktop/projects/website/website/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
       options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/robot-icon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"6794f3474f9c225014bbb3da5cc01e4f"},
     }]
