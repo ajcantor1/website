7a13faff3ec28de6db3dd7f6502a64db9204c3c0
jeremy <ajcantor1@gmail.com>
2021-06-30 12:32:45 -0400

    static query fix

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/jeremy/Desktop/projects/website/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/home/jeremy/Desktop/projects/website/src/pages/404.js"))),
   "component---src-pages-gallery-js": hot(preferDefault(require("/home/jeremy/Desktop/projects/website/src/pages/gallery.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/home/jeremy/Desktop/projects/website/src/pages/index.js"))),
