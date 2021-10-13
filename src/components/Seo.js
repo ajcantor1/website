e5bbca2a6c33c819c043f0c6aaa2891d884dff26
jeremy <ajcantor1@gmail.com>
2021-10-13 16:02:53 -0400

    Removed static query from website meta

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -1,73 +1,59 @@
 
 import React from "react"
 import { Helmet } from "react-helmet"
-import { StaticQuery, graphql } from "gatsby"
 
 
-const Seo = ({description, keywords, title, image, url, author}) => {
+const metaDescription = "Smart automated systems sleek design"
+const metaTitle = "Cantor Computing"
+const metaAuthor = "Jeremy Cantor"
+const metaUrl = "https://www.cantorcomputing.com"
+const metaImage = `src/images/robot-icon.png`
+const metaKeywords =  ["web design", "deep learning", "software", "development", "machine learning"]
+
+const Seo = () => {
   return (
-    <StaticQuery query={
-      graphql`
-        query Seo {
-          site {
-            siteMetadata {
-              title
-              description
-              url
-              author
-              image
-          }
-        }         
+
+
+  <Helmet
+    title={metaTitle}
+    meta={[
+      {
+        name: `description`,
+        content: metaDescription
+      },
+      {
+        property: `og:title`,
+        content: metaTitle
+      },
+      {
+        property: `og:author`,
+        content: metaAuthor
+      },
+      {
+        property: `og:description`,
+        content: metaDescription
+      }, 
+      {
+        property: `og:type`,
+        content: `website`
+      },
+      {
+        property: `og:image`,
+        content: metaImage
+      },
+      {
+        property: `og:url`,
+        content: metaUrl
       }
-    `}
-    render={data => {
-      const metaDescription = description || data.site.siteMetadata.description
-      const metaTitle = title || data.site.siteMetadata.title
-      const metaAuthor = author || data.site.siteMetadata.author
-      const metaUrl = url || data.site.siteMetadata.url
-      const metaImage = image || data.site.siteMetadata.image
-      const metaKeywords = keywords || ["web design", "deep learning", "software", "development"]
-      return (
-        <Helmet
-          title={metaTitle}
-          meta={[
-            {
-              name: `description`,
-              content: metaDescription
-            },
-            {
-              property: `og:title`,
-              content: metaTitle
-            },
-            {
-              property: `og:author`,
-              content: metaAuthor
-            },
-            {
-              property: `og:description`,
-              content: metaDescription
-            }, 
-            {
-              property: `og:type`,
-              content: `website`
-            },
-            {
-              property: `og:image`,
-              content: metaImage
-            },
-            {
-              property: `og:url`,
-              content: metaUrl
-            }
-          ].concat(
-            metaKeywords && metaKeywords.length > 0 ? {
-              name: `keyword`,
-              content: metaKeywords.join(`, `)
-            } : []
-          )}
-        />
-      )
-    }}/>
-  )
+    ].concat(
+      metaKeywords && metaKeywords.length > 0 ? {
+        name: `keyword`,
+        content: metaKeywords.join(`, `)
+      } : []
+    )}
+  />
+)
+
+  
 }
 export default Seo
