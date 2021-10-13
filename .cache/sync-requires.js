1e3253aa930ce7f00af998c382b9974dd0887cd1
jeremy <ajcantor1@gmail.com>
2021-10-13 15:24:30 -0400

    Fix heading text width

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/home/jeremy/Desktop/projects/website/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/home/jeremy/Desktop/projects/website/src/pages/404.js")),
   "component---src-pages-gallery-js": preferDefault(require("/home/jeremy/Desktop/projects/website/src/pages/gallery.js")),
   "component---src-pages-index-js": preferDefault(require("/home/jeremy/Desktop/projects/website/src/pages/index.js")),
