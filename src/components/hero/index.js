49008cf51d4082435d3f52527ca2e69d8cda87b6
jeremy <ajcantor1@gmail.com>
2023-06-17 13:02:23 -0400

    add thesis

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -25,6 +25,7 @@ import arduino from '../../images/arduino.png';
 import devices from '../../images/devices.png';
 import detection from '../../images/detection.png';
 import seaquest from '../../images/seaquest.gif';
+import thesis from '../../images/thesis.gif';
 import wordle_bot from '../../images/wordle_bot.gif';
 import switchgame from '../../images/switch.gif';
 import robotpath from '../../images/robot_path.gif';
@@ -63,7 +64,7 @@ const Hero = () => {
       <PortfolioSection>
 
       <PortfolioBox>
-          <PortfolioImage src={detection}/>
+          <PortfolioImage src={thesis}/>
           <PortfolioDescription>
             <PortfolioTitle>
               Master's Thesis
@@ -76,7 +77,7 @@ const Hero = () => {
           </PortfolioDescription>
         </PortfolioBox>
 
-        <PortfolioBox>
+        <PortfolioBox className="reverse">
           <PortfolioImage src={detection}/>
           <PortfolioDescription>
             <PortfolioTitle>
@@ -91,7 +92,7 @@ const Hero = () => {
           </PortfolioDescription>
         </PortfolioBox>
 
-        <PortfolioBox className="reverse">
+        <PortfolioBox>
         <PortfolioImage src={seaquest}/>
           <PortfolioDescription>
             <PortfolioTitle>
@@ -105,7 +106,7 @@ const Hero = () => {
           </PortfolioDescription>
         </PortfolioBox>
 
-        <PortfolioBox>
+        <PortfolioBox className="reverse">
         <PortfolioImage src={wordle_bot}/>
           <PortfolioDescription>
             <PortfolioTitle>
@@ -120,7 +121,7 @@ const Hero = () => {
           </PortfolioDescription>
         </PortfolioBox>
 
-        <PortfolioBox className="reverse">
+        <PortfolioBox>
         <PortfolioImage src={switchgame}/>
           <PortfolioDescription>
             <PortfolioTitle>
@@ -135,7 +136,7 @@ const Hero = () => {
           </PortfolioDescription>
         </PortfolioBox>
 
-        <PortfolioBox>
+        <PortfolioBox className="reverse">
         <PortfolioImage src={robotpath}/>
           <PortfolioDescription>
             <PortfolioTitle>
